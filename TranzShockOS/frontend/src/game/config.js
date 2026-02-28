2a1d2b2498b0cce2db4d89769af2281b928e5513
rdeluciobeatz-maker <rdeluciobeatz@gmail.com>
2026-02-28 01:09:28 -0300

    Create config.js

---
+++ TranzShockOS/frontend/src/game/config.js
@@ -0,0 +1,13 @@
+export const gameConfig = {
+  width: 800,
+  height: 600,
+  tileSize: 32,
+  mapWidth: 25,
+  mapHeight: 18,
+  colors: {
+    floor: 0x2a3a2a,
+    wall: 0x1a2a1a,
+    agent1: 0x7fff7f,
+    agent2: 0x5ff0ff
+  }
+};
